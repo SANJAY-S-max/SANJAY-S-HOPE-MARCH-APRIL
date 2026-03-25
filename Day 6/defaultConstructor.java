5c050c55a11a0be09dd56db9741099ddd687648c
SANJAY-S-max <sanjayvision125@gmail.com>
2026-03-25 11:14:44 +0530

    test3

--- Day 6/defaultConstructor.java
+++ Day 6/defaultConstructor.java
@@ -1,3 +1,6 @@
 public class defaultConstructor {
-    
+    public static void main(String[] args) {
+        // creating an object of the class
+        System.out.println("I am inside the default constructor");
+    }
 }
